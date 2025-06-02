bc5aa9a669ecd973c9a3ad0a8c59808bcd7cc770
Yash sharma <60930724+Yashsharma365@users.noreply.github.com>
2025-06-02 22:17:53 +0530

    Checks

--- section2/loans/src/main/java/com/eazybytes/loans/mapper/LoanMapper.java
+++ section2/loans/src/main/java/com/eazybytes/loans/mapper/LoanMapper.java
@@ -2,7 +2,6 @@ package com.eazybytes.loans.mapper;
 
 import com.eazybytes.loans.dto.LoansDto;
 import com.eazybytes.loans.entity.Loans;
-
 public class LoanMapper {
     public static LoansDto mapToLoanDto(Loans loans, LoansDto loansDto){
         loansDto.setLoanType(loans.getLoanType());
